0dc305c00ab8176a269960b02eb36da574b9dce3
rionguyenfcs <hau.nguyen@floatingcube.com>
2026-03-13 03:18:39 +0000

    update new zippy core

--- src/wp-content/themes/zippy-elementor-child/inc/hooks/woocommerce.php
+++ src/wp-content/themes/zippy-elementor-child/inc/hooks/woocommerce.php
@@ -1,29 +1,29 @@
 <?php
 
-add_filter('woocommerce_email_enabled', '__return_false');
+// add_filter('woocommerce_email_enabled', '__return_false');
 
-function generate_test_orders($count = 1000) {
+// function generate_test_orders($count = 1000) {
 
-    for ($i = 0; $i < $count; $i++) {
+//     for ($i = 0; $i < $count; $i++) {
 
-        $order = wc_create_order();
+//         $order = wc_create_order();
 
-        $product_id = 52;
-        $product = wc_get_product($product_id);
+//         $product_id = 52;
+//         $product = wc_get_product($product_id);
 
-        $order->add_product($product, rand(1,3));
+//         $order->add_product($product, rand(1,3));
 
-        $order->set_address([
-            'first_name' => 'Test',
-            'last_name'  => 'User'.$i,
-            'email'      => "test$i@email.com",
-        ], 'billing');
+//         $order->set_address([
+//             'first_name' => 'Test',
+//             'last_name'  => 'User'.$i,
+//             'email'      => "test$i@email.com",
+//         ], 'billing');
 
-        $order->calculate_totals();
-        $order->update_status('completed');
+//         $order->calculate_totals();
+//         $order->update_status('completed');
 
-    }
+//     }
 
-}
+// }
 
 // generate_test_orders(1000);
